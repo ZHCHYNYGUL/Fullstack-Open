0ac4b1a6d08bfacade388207abde6963fd09b5c5
ZHCHYNYGUL <minakim2503@gmail.com>
2022-07-08 19:19:44 +0300

    07.07.22

---
+++ src/Components/Header/Header.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import App from '../../App';
+const Header = (props) => {
+    return (
+        <div>
+            <h1>{props.course}</h1>
+        </div>
+    );
+};
+
+export default Header;
